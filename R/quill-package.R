f299f1d9cb1369ab35f457b026b8efc13cd0e953
svalvaro <sanchezvillalba.alvaro@gmail.com>
2024-11-12 17:48:08 +0100

    feat: Added imports and export

---
+++ R/quill-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import htmlwidgets
+#' @import rlang
+#' @importFrom jsonlite toJSON
+#' @importFrom purrr map2 discard set_names
+## usethis namespace: end
+NULL
